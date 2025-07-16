78a66c49fdaeda6bf36a0f4912e522a632d3131b
Arsalan-05 <arsalan.amir.ali05@gmail.com>
2025-07-16 09:33:29 -0400

    Fix merge conflicts

---
+++ ios/App/App/ViewControllers/FilterViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+import SwiftUI
+
+class FilterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        let filterView = FilterView()
+        let hostingController = UIHostingController(rootView: filterView)
+        
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.didMove(toParent: self)
+        
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
