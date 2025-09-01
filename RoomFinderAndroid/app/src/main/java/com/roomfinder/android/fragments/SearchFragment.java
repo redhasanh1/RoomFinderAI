3b90364c40b95394dea38987e809add9d37781c7
RoomFinder Dev <dev@roomfinder.ai>
2025-09-01 00:20:06 +0000

    fix search compilation errors

--- RoomFinderAndroid/app/src/main/java/com/roomfinder/android/fragments/SearchFragment.java
+++ RoomFinderAndroid/app/src/main/java/com/roomfinder/android/fragments/SearchFragment.java
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
